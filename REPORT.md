5a1c6e3f8e708662da75f8fe1789f7909eb8f5c7
ediaz25 <ediaz25@miners.utep.edu>
2020-12-02 23:51:13 +0000

    completed

--- REPORT.md
+++ REPORT.md
@@ -1,6 +1,6 @@
-##### Name: (Write your name here)
-##### Date: (Write the date here)
+##### Name: Eduardo Diaz
+##### Date: October 1
 
 ### CODING CHALLENGE #2: REPORT
 
-(Write your report here)
+During this coding challenge a total of 5 if statements were used unless the else statement counts as well then 6. I was able to combine the 2 if statements given by using the and and or functions as well as every consecutive if statement after that one. To one up my design structure of if statements there has to be a way to combine all the values into one huge if statement but it is difficult to complete it with the proper syntax. 
--- main.c
+++ main.c
@@ -16,9 +16,24 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
-      printf("Normal\n");
+  if(s < d){ // outputs error if the diastolic pressure is bigger than the systolic
+    printf("ERROR\n");
+    return 0;
+  }
+  if(s < 120 && d < 80) { // checks if systolic and diastolic are normal levels
+    printf("Normal\n");  
+  }
+  else if(s >= 120 && s <= 129 && d < 80){ // checks if systolic and diastolic are elevated levels
+    printf("Elevated\n");
+  }
+  else if((s >= 130 && s <= 139) || (d >= 80 && d <= 89)){ // checks if systolic and diastolic are stage 1
+    printf("Stage 1 Hypertension");
+  }
+  else{ // checks if systolic and diastolic are stage 2 and beyond 
+    printf("Stage 2 Hypertension\n");
+    if(s > 180 || d > 120){
+      printf("Hypertensive Crysis");
     }
   }
+  return 0;
 }
